b4127d769172312857b52f27e35d7e7980ee891b
Mary West <westmarye1219@gmail.com>
2018-10-18 21:46:38 -0500

    added locations json

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -1,1 +1,20 @@
-console.log('locationComponenets.js');
+// const writeFishes = (arrayOfLocations) => {
+//     let domString = '';
+//     arrayOfFishes.forEach((fish) => {
+//         domString += `
+//         <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3"> 
+//         <div class="thumbnail">
+//             <img src="${fish.imageSoure}" alt="" width="40%">
+//             <div class="caption">
+//                 <h3 id="thumbnail-label">${fish.name}</h3>
+//                 <p>$
+//                     <span class="price">${fish.basePrice}</span>
+//                 </p>
+//             </div>
+//             <div class="caption card-footer">
+//                 <button class="add btn btn-danger">Add To Basket</button>
+//             </div>
+//         </div>
+//         </div>`
+
+//     });
