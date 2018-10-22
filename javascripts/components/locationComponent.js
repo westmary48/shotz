0aeb2a0a70cdaa8fb0aba0cff5625b36b194ccff
Mary West <westmarye1219@gmail.com>
2018-10-21 20:41:32 -0500

    added locations string builder

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -1,20 +1,18 @@
-// const writeFishes = (arrayOfLocations) => {
-//     let domString = '';
-//     arrayOfFishes.forEach((fish) => {
-//         domString += `
-//         <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3"> 
-//         <div class="thumbnail">
-//             <img src="${fish.imageSoure}" alt="" width="40%">
-//             <div class="caption">
-//                 <h3 id="thumbnail-label">${fish.name}</h3>
-//                 <p>$
-//                     <span class="price">${fish.basePrice}</span>
-//                 </p>
-//             </div>
-//             <div class="caption card-footer">
-//                 <button class="add btn btn-danger">Add To Basket</button>
-//             </div>
-//         </div>
-//         </div>`
+const locationStringBuilder = (arrayOfLocation) => {
+    let domString = '';
+    arrayOfLocation.forEach((location) => {
+        domString += `
+        <div class="${location.shooTtime} location card col-md-6 col-md-offset-3"> 
+        <div class="thumbnail">
+            <img src="${location.locationImage}" alt="" width="40%">
+            <div class="caption">
+                <h3 id="thumbnail-label">${location.name}</h3>
+                <p>$
+                    <span class="price">${location.locationAddresa}</span>
+                </p>
+            </div>
+        </div>
+        </div>`
 
-//     });
+    })
+}
