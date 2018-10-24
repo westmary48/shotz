a98eda507a0182a7d3af5683c8e20d67807cb34f
Mary West <westmarye1219@gmail.com>
2018-10-23 20:55:56 -0500

    json, stringbuilder

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -1,4 +1,35 @@
-const locationStringBuilder = (arrayOfLocation) => {
+
+let locations = [];
+
+const setLocations = (newArray) => {
+    pets = newArray;
+};
+
+const getLocationz = () => {
+    return locations;
+};
+
+const sortLocationa = (e) => {
+    const shootTime = e.target.id;
+    if (shootTime === 'all') {
+        LocationBuilder(locations);
+    } else if (shootTime === 'Morning') {
+        const filteredMorning = locations.filter(x => x.shootTime === shootTime);
+        locationsBuilder(filteredMorning);
+    } else if (shootTime === 'Afternoon') {
+        const filteredAfternoon = locations.filter(x => x.shootTime === shootTime);
+        locationsBuilder(filteredAfternoon);
+    } else if (shootTime === 'Evening') {
+        const filteredEvening = locations.filter(x => x.shootTime === shootTime);
+        locationsBuilder(filteredEvening);
+    } else if (shootTime === 'After Dark') {
+        const filteredAfterDark = locations.filter(x => x.shootTime === shootTime);
+        locationsBuilder(filteredAfterDark);
+    }
+};
+
+
+const locationBuilder = (arrayOfLocation) => {
     let domString = '';
     arrayOfLocation.forEach((location) => {
         domString += `
@@ -8,11 +39,13 @@ const locationStringBuilder = (arrayOfLocation) => {
             <div class="caption">
                 <h3 id="thumbnail-label">${location.name}</h3>
                 <p>$
-                    <span class="price">${location.locationAddresa}</span>
+                    <span class="price">${location.address}</span>
                 </p>
             </div>
+            <div class="caption card-footer">
+            <div class="${shootTime}"></div>
         </div>
         </div>`
-
     })
-}
+
+    }
